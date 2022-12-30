ed35f468b5b1e28719d16f5de1e05703fb77c9b1
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2022-12-30 21:54:37 +0200

    Init

---
+++ CosmosExplorer.Domain/Connection/ConnectionService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace CosmosExplorer.Domain;
+
+public class ConnectionService : IConnectionService
+{
+    public async Task<string> GetDataAsync()
+    {
+        CosmosClient cosmosClient = new CosmosClient(
+            "",
+            "");
+
+        Database db = cosmosClient.GetDatabase("hometraining");
+        Container container =  db.GetContainer("HomeTrainingContext");
+        return container.Id;
+    }
+}
+
---
+++ CosmosExplorer.Domain/Connection/IConnectionService.cs
@@ -0,0 +1,7 @@
+﻿namespace CosmosExplorer.Domain;
+
+public interface IConnectionService
+{
+    public Task<string> GetDataAsync();
+}
+
